c24f5929f3fc08a7b092251c5b04d3c9ebd7b83f
Daenoor <da3n00r@gmail.com>
2010-07-22 18:17:35 +0700

    frontend module skeleton

---
+++ modules/ikCatalogue/actions/actions.class.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/BaseikCatalogueActions.class.php';
+
+/**
+ * ikCatalogue actions.
+ * 
+ * @package    iktomiCataloguePlugin
+ * @subpackage ikCatalogue
+ * @author     Andrey Yaroshenko <da3n00r@gmail.com>
+ * @version    SVN: $Id: actions.class.php 12534 2008-11-01 13:38:27Z Kris.Wallsmith $
+ */
+class ikCatalogueActions extends BaseikCatalogueActions
+{
+}
---
+++ modules/ikCatalogue/lib/BaseikCatalogueActions.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Base actions for the iktomiCataloguePlugin ikCatalogue module.
+ * 
+ * @package     iktomiCataloguePlugin
+ * @subpackage  ikCatalogue
+ * @author      Andrey Yaroshenko <da3n00r@gmail.com>
+ * @version     SVN: $Id: BaseActions.class.php 12534 2008-11-01 13:38:27Z Kris.Wallsmith $
+ */
+abstract class BaseikCatalogueActions extends sfActions
+{
+}
